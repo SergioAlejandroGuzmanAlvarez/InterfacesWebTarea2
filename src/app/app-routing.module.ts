a31c2c2d4a531b91a30866c8d4255ae8bc6b53e2
Sergio <contact@sealguzmanal@ittepic.edu.mx>
2023-02-09 22:25:18 -0700

    V.1.0 WebTerminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CuadroComponent } from './components/cuadro/cuadro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio',component: InicioComponent},
+  {path: 'cuadro',component: CuadroComponent},
+  {path : '**', redirectTo: 'inicio',pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CuadroComponent } from './components/cuadro/cuadro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    CuadroComponent
   ],
   imports: [
     BrowserModule,
